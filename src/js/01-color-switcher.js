070ef010bfecb67e92a1377eb6ae7981db432c01
MateuszBachor <mateuszbachor3@gmail.com>
2022-06-28 00:20:28 +0200

    asd
    asd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
-
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button")
+const stopBtn = document.querySelector("#stopBtn")
+let timerId = null;
+// startBtn.style.margin="50%"
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  startBtn.addEventListener("click", () => {
+        startBtn.setAttribute("disabled","" )
+   timerId = setInterval(() => {
+        body.style.background = getRandomHexColor();
+      }, 1000);
+});
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+startBtn.removeAttribute("disabled")
+  });
