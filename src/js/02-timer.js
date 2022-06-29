a7fd3d6a7dfc3506f22de5f38466282f0085f10e
MateuszBachor <mateuszbachor3@gmail.com>
2022-06-29 07:16:24 +0200

    finish?

---
+++ src/js/02-timer.js
@@ -0,0 +1,52 @@
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
+ let datetimePicker = document.querySelector("#datetime-picker")
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= new Date()) {
+          startBtn.disabled = true;
+          window.alert('Please choose a date in the future');
+        } else {
+          startBtn.disabled = false;
+
+         function countDown(){
+            startBtn.disabled = true;
+           timer = setInterval(() => {
+            let dateChosen = datetimePicker.getAttribute("value")
+            dateChosen = new Date(dateChosen)
+            let now = new Date()
+            let diff = dateChosen - now;
+           const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+           const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+           const m = Math.floor(diff / 1000 / 60 ) % 60;
+           const s = Math.floor(diff / 1000) % 60;
+
+           days.innerHTML = d;
+           hours.innerHTML = h < 10 ? '0' + h : h;
+           minutes.innerHTML = m < 10 ? '0' + m : m;
+           seconds.innerHTML = s < 10 ? '0' + s : s;
+         if (diff < 1000) {
+            clearInterval(timer);
+            startBtn.disabled = false;
+          }
+        }, 1000);
+      }
+          startBtn.addEventListener('click', countDown);
+        }
+    }
+} 
+flatpickr("#datetime-picker", options);
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+const form = document.querySelector('.form');
+
+const delayd = document.querySelector('[name="delay"]');
+const stepd = document.querySelector('[name="step"]');
+const amountd = document.querySelector('[name="amount"]');
+
+
+
+function idk(event) {
+
+  event.preventDefault();
+
+  let delay = parseInt(delayd.value,10)
+  const step = parseInt(stepd.value,10)
+  const amount = parseInt(amountd.value,10)
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(
+          `✅ Fulfilled promise ${i} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          `❌ Rejected promise ${i} in ${delay}ms`
+        );
+      });
+    delay = delay + step;
   }
 }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', idk);
